644ae24e880cb86bb1a4e31f32ff6c2b3c715133
pelegsch666 <peleg.mazuz@gmail.com>
2023-10-24 16:52:03 +0300

    done

--- src/components/core/level-link/levelLink.tsx
+++ src/components/core/level-link/levelLink.tsx
@@ -10,27 +10,27 @@ type Props = {
 function LevelLink({ url, state, level }: Props) {
   return (
     (state === 'locked' && (
-      <div className="w-5 h-5 rounded-full bg-gray-500 text-white text-center flex justify-center items-center mt-[200px]">
+      <div className="w-5 h-5 rounded-full bg-gray-500 text-white text-center flex justify-center items-center">
         {level}
       </div>
     )) ||
     (state === 'failed' && (
       <Link
-        className="w-5 h-5 rounded-full bg-red-500 text-white text-center flex justify-center items-center mt-[200px]"
+        className="w-5 h-5 rounded-full bg-red-500 text-white text-center flex justify-center items-center"
         href={url}>
         {level}
       </Link>
     )) ||
     (state === 'current' && (
       <Link
-        className="w-5 h-5 rounded-full bg-yellow-500 text-white text-center flex justify-center items-center mt-[200px]"
+        className="w-5 h-5 rounded-full bg-yellow-500 text-white text-center flex justify-center items-center"
         href={url}>
         {level}
       </Link>
     )) ||
     (state === 'passed' && (
       <Link
-        className="w-5 h-5 rounded-full bg-green-500 text-white text-center flex justify-center items-center mt-[200px]"
+        className="w-5 h-5 rounded-full bg-green-500 text-white text-center flex justify-center items-center"
         href={url}>
         {level}
       </Link>
