19efa5da19699200dd66367e7c1b03d18b71fe3a
Irad Doron <irad.doron@gmail.com>
2023-10-26 12:53:18 +0300

    add style theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,59 @@ module.exports = {
 					400: '#858EAD',
 				},
 				'accent-blue': '#1DA1F2',
+				light: {
+					background: {
+						default: '#FFFFFF',
+						paper: '#F4F6F8',
+					},
+					primary: {
+						main: '#7785FF',
+						light: '#FFF1E6',
+						dark: '#7785FF',
+						contrastText: '#FFFFFF',
+					},
+					gradients: {
+						peach: 'linear-gradient(208deg, #E31F76 -13.56%, #792B8B 91.39%)',
+						lambada: 'linear-gradient(208deg, #66E175 -20.27%, #5387E9 95.68%)',
+					},
+					boxShadow: {
+						large: '0px 4px 120px 0px rgba(50, 54, 101, 0.25)',
+					},
+				},
+				dark: {
+					background: {
+						default: '#323665',
+						paper: '#212734',
+					},
+					primary: {
+						main: '#79DFFF',
+						light: '#BDEFFF',
+						dark: '#00B0E8',
+						contrastText: '#000000',
+					},
+					gradients: {
+						peach: 'linear-gradient(208deg, #FFAED3 -13.56%, #FF5E98 91.39%)',
+						lambada: 'linear-gradient(208deg, #B9FFC1 -20.27%, #8CB4FF 95.68%)',
+					},
+					boxShadow: {
+						large: '0px 4px 120px 0px rgba(255, 255, 255, 0.15)',
+					},
+				},
+				common: {
+					greyScale: {
+						100: '#f1f1f1',
+						200: '#d4d4d4',
+						300: '#b8b8b8',
+						400: '#9c9c9c',
+						500: '#808080',
+						600: '#636363',
+						700: '#474747',
+						800: '#2a2a2a',
+						900: '#2a2a2a',
+					},
+					white: '#FFFFFF',
+					black: '#000000',
+				},
 			},
 			fontFamily: {
 				inter: ['var(--font-inter)'],
