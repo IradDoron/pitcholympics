a2085e937a4ee9329193ec6406ebe78c8886609e
pelegsch666 <peleg.mazuz@gmail.com>
2023-10-29 21:11:47 +0200

    add logic

--- src/components/core/button/Button.tsx
+++ src/components/core/button/Button.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 type Props = {
     label: string;
+    onClick: () => void;
 }
 
-const Button = ({label}: Props) => {
+const Button = ({label,onClick}: Props) => {
   return (
-    <button className='px-6 py-3 bg-gradient-to-r from-purple-600 to-indigo-600 text-white rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition-transform duration-300 hover:bg-indigo-700 hover:text-white'>
+    <button className='px-6 py-3 bg-gradient-to-r from-purple-600 to-indigo-600 text-white rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition-transform duration-300 hover:bg-indigo-700 hover:text-white' onClick={onClick}>
     {label}
     </button>
   )
