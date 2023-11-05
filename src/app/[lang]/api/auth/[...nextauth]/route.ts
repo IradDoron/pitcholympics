b14c1a659fc48126f6a282f2eff7d8ec64185921
Irad Doron <irad.doron@gmail.com>
2023-11-05 16:20:24 +0200

    comment route file

--- src/app/[lang]/api/auth/[...nextauth]/route.ts
+++ src/app/[lang]/api/auth/[...nextauth]/route.ts
@@ -2,26 +2,26 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions: NextAuthOptions = {
-	secret: process.env.NEXTAUTH_SECRET,
+// export const authOptions: NextAuthOptions = {
+// 	secret: process.env.NEXTAUTH_SECRET,
 
-	providers: [
-		GitHubProvider({
-			clientId: process.env.GITHUB_ID ?? '',
-			clientSecret: process.env.GITHUB_SECRET ?? '',
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-		}),
-	],
-	callbacks: {
-		async session(params) {
-			return params.session;
-		},
-	},
-};
+// 	providers: [
+// 		GitHubProvider({
+// 			clientId: process.env.GITHUB_ID ?? '',
+// 			clientSecret: process.env.GITHUB_SECRET ?? '',
+// 		}),
+// 		GoogleProvider({
+// 			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+// 			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+// 		}),
+// 	],
+// 	callbacks: {
+// 		async session(params) {
+// 			return params.session;
+// 		},
+// 	},
+// };
 
-export const handler = NextAuth(authOptions);
+// export const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
