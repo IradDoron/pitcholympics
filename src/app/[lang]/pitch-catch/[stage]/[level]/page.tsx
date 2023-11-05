777e90eb772a10e78865c53baaaa744c1a696709
pelegsch666 <peleg.mazuz@gmail.com>
2023-11-05 19:07:02 +0200

    done

--- src/app/[lang]/pitch-catch/[stage]/[level]/page.tsx
+++ src/app/[lang]/pitch-catch/[stage]/[level]/page.tsx
@@ -3,11 +3,13 @@
 import { useState, useEffect } from 'react';
 import NoteStep from '@/components/shared/noteStep';
 import { PitchCatchGame, PitchCatchLevel } from '@/types';
-import memoTheMeloMockData from '@/mockData/memoTheMelo';
+
 import PitchButton from '@/components/shared/pitchButton';
 import UserOptions from '@/components/shared/userOption';
 import Button from '@/components/core/button';
 import pitchCatchData from '@/mockData/pitchCatch';
+
+import { useRouter } from 'next/navigation';
 
 type Props = {
   params: {
@@ -20,7 +22,6 @@ const getLevelData = (
   level: number,
   levelsData: PitchCatchGame
 ) => {
-
   return levelsData[stage - 1][level - 1];
 };
 
@@ -33,10 +34,17 @@ const Page = ({ params }: Props) => {
     Number(level),
     pitchCatchData
   );
-
-  // const [currPitch, setCurrPitch] = useState(
-  //   currentLevel[currQuestion - 1].currPitch
-  // );
+  const router = useRouter();
+  const handleWin = () => {
+    const scoreWinning = params.stage + params.level * 2;
+    console.log('iam here handleWin');
+    localStorage.setItem('score', scoreWinning.toString());
+    // redirect(`${params.level}/result`);
+    router.push(`${params.level}/result`);
+  };
+  const handleLose = () => {
+    router.push(`${params.level}/result`);
+  };
 
   function arrCheck(arrOne: number[], arrTwo: number[]) {
     for (let i = 0; i < arrOne.length; i++) {
@@ -67,18 +75,14 @@ const Page = ({ params }: Props) => {
     );
 
     if (isCorrect) {
-
-      if (currQuestion <= currentLevel.length - 1) {
+      if (currQuestion < currentLevel.length) {
         setCurrQuestion(currQuestion + 1);
-   
+      } else {
+        handleWin();
       }
-      if (currQuestion > currentLevel.length - 1) {
-      
-      }
-
-      console.log('currquestion', currQuestion);
     } else {
-      console.log('move to result page');
+      localStorage.setItem('score', '0'); 
+      handleLose();
     }
   }
   return (
