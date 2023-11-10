f35cc47a483b5109ea1f85a33e54c65e7fd4d93d
Irad Doron <irad.doron@gmail.com>
2023-11-10 11:03:36 +0200

    add peleg image

--- src/data/collaborators.ts
+++ src/data/collaborators.ts
@@ -3,11 +3,13 @@ import { StaticImageData } from 'next/image';
 import IRAD_DORON from 'public/assets/collaborators/irad-doron.png';
 import AMIT_HALEVI from 'public/assets/collaborators/amit-halevi.png';
 import GAL_EIN_DOR from 'public/assets/collaborators/gal-ein-dor.png';
+import PELEG_MAZUZ from 'public/assets/collaborators/peleg-mazuz.png';
 
 const CollaboratorsImages: { [key: string]: StaticImageData } = {
 	'irad-doron': IRAD_DORON,
 	'amit-halevi': AMIT_HALEVI,
 	'gal-ein-dor': GAL_EIN_DOR,
+	'peleg-mazuz': PELEG_MAZUZ,
 };
 
 const collaborators: Collaborator[] = [
@@ -53,7 +55,7 @@ const collaborators: Collaborator[] = [
 		roles: ['full-stack-developer'],
 		city: 'Ramle',
 		lookingFor: 'Looking for Full Stack Developer Position',
-		image: CollaboratorsImages['gal-ein-dor'],
+		image: CollaboratorsImages['peleg-mazuz'],
 		linkedin: 'https://www.linkedin.com/in/gal-eindor/',
 		github: 'https://github.com/pelegsch666',
 	},
