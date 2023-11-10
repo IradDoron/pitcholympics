7841a9d2d03af4462b242c87c18f964d59114a23
Irad Doron <irad.doron@gmail.com>
2023-11-10 10:38:51 +0200

    fix colors bug

--- src/components/shared/collaboratorCard/Image.tsx
+++ src/components/shared/collaboratorCard/Image.tsx
@@ -8,14 +8,18 @@ type Props = {
 	color: CardComponentsColor;
 };
 
-const getStyles = (color: CardComponentsColor) => {
-	const styles = `flex justify-center border-[8px] rounded-[50%] border-light-${color}-main dark:border-dark-${color}-main`;
-	return styles;
-};
-
 const Image = ({ image, alt, color }: Props) => {
+	const styles = {
+		primary:
+			'flex justify-center border-[8px] rounded-[50%] border-light-primary-main dark:border-dark-primary-main',
+		secondary:
+			'flex justify-center border-[8px] rounded-[50%] border-light-secondary-main dark:border-dark-secondary-main',
+		tertiary:
+			'flex justify-center border-[8px] rounded-[50%] border-light-tertiary-main dark:border-dark-tertiary-main',
+	};
+
 	return (
-		<div className={` ${getStyles(color)}`}>
+		<div className={styles[color]}>
 			<NextImage src={image} alt={alt} width={200} height={200} />
 		</div>
 	);
