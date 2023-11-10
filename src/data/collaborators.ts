a2ee69a3ee5f241c0e274a4caddf5de40bfaa65a
Irad Doron <irad.doron@gmail.com>
2023-11-10 10:29:01 +0200

    add gal photo

--- src/data/collaborators.ts
+++ src/data/collaborators.ts
@@ -2,10 +2,12 @@ import { Collaborator } from '@/types';
 import { StaticImageData } from 'next/image';
 import IRAD_DORON from 'public/assets/collaborators/irad-doron.png';
 import AMIT_HALEVI from 'public/assets/collaborators/amit-halevi.png';
+import GAL_EIN_DOR from 'public/assets/collaborators/gal-ein-dor.png';
 
 const CollaboratorsImages: { [key: string]: StaticImageData } = {
 	'irad-doron': IRAD_DORON,
 	'amit-halevi': AMIT_HALEVI,
+	'gal-ein-dor': GAL_EIN_DOR,
 };
 
 const collaborators: Collaborator[] = [
@@ -32,6 +34,17 @@ const collaborators: Collaborator[] = [
 		linkedin: 'https://www.linkedin.com/in/amit-halevi',
 		github: 'https://github.com/HaleviAmit',
 		portfolio: 'http://www.haleviamit.com/',
+	},
+	{
+		id: '3',
+		firstName: 'Gal',
+		lastName: 'Ein Dor',
+		roles: ['full-stack-developer'],
+		city: 'Tel Aviv',
+		lookingFor: 'Looking for Full Stack Developer Position',
+		image: CollaboratorsImages['gal-ein-dor'],
+		linkedin: 'https://www.linkedin.com/in/gal-eindor/',
+		github: 'https://github.com/galeindor',
 	},
 ];
 
