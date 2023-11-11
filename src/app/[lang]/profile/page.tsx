c1c2b4a69d8669139e183af4e2f611764dcc08ad
DoovDevan <haleviamit@gmail.com>
2023-11-11 15:09:13 +0200

    User Image shared

--- src/app/[lang]/profile/page.tsx
+++ src/app/[lang]/profile/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Card from '@/components/core/card'
+import UserImage from '@/components/shared/userImage'
 
 const page = () => {
+
     return (
-        <div>Profile page</div>
+        <div className='flex items-center justify-center h-full'>
+            <Card color='primary' className='flex flex-col' shadow='large' style={{ width: '330px' }}>
+                <UserImage />
+
+            </Card>
+
+        </div>
     )
 }
 
