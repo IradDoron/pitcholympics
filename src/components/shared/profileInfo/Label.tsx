4840cbaa972f6c98f5c3f69b32034229d767bee2
Irad Doron <irad.doron@gmail.com>
2023-11-21 21:04:32 +0200

    add hebrew to profile page

--- src/components/shared/profileInfo/Label.tsx
+++ src/components/shared/profileInfo/Label.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 type Props = {
     label: string;
     children: React.ReactNode;
+    className?: string;
 };
 
-const Label = ({ label, children }: Props) => {
+const Label = ({ label, children, className = '' }: Props) => {
     return (
-        <label className='w-full'>
+        <label
+            className={`w-full text-light-surface-onPrimary dark:text-dark-surface-onPrimary ${className}`}>
             <span>{label}:</span>
             {children}
         </label>
