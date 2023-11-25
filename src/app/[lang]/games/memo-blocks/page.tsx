a731f7ec90731f099fe620c6de89cf900bee51d0
Asaf Levi <asafl14@yahoo.com>
2023-11-25 18:42:18 +0200

    delete commented lines in page - memo-block

--- src/app/[lang]/games/memo-blocks/page.tsx
+++ src/app/[lang]/games/memo-blocks/page.tsx
@@ -1,11 +1,8 @@
 import GameLevelsLinks from '@/components/shared/gameLevelsLinks';
-//import MemoBlock from '@/components/shared/memoBlocks/MemoBlock';
 import memoTheMeloMockData from '@/mockData/memoTheMelo';
 import { LangParam } from '@/types';
 import { getDictionaryServer } from '@/utils/getDictionaryServer';
-import MemoBlock from '@/components/shared/memoBlocksAsaf/MemoBlock';
 import MemoGame from '@/components/shared/memoBlocksAsaf/MemoGame';
-import Test from '@/components/shared/memoBlocksAsaf/Test';
 
 const Page = async ({ params: { lang } }: LangParam) => {
     const dict = await getDictionaryServer(lang);
