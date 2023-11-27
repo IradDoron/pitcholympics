0cc2e42b633951d6afacd4acbec7297290d07adc
gal <gal.eindor@gmail.com>
2023-11-27 18:00:10 +0200

    more requested changes done refactor folder name removed unused dictionary ( to be used in the future)

--- src/app/[lang]/games/memo-blocks/page.tsx
+++ src/app/[lang]/games/memo-blocks/page.tsx
@@ -1,12 +1,7 @@
-
 import { LangParam } from '@/types';
-import { getDictionaryServer } from '@/utils/getDictionaryServer';
-import MemoBlocksGame from '@/components/shared/memoBlocks';
+import MemoBlocksGame from '@/components/shared/memoBlocksGame';
 
 const Page = async ({ params: { lang } }: LangParam) => {
-    const dict = await getDictionaryServer(lang);
-    const { page } = dict.app.contact;
-
     return (
         <div className='h-full flex justify-center items-center'>
             <MemoBlocksGame />
