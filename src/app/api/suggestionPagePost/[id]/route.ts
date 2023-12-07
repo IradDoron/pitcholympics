83f83e12e2d8ec65ed5b94b0c08de26422483f8d
pelegsch666 <peleg.mazuz@gmail.com>
2023-12-07 10:06:41 +0200

    finally connected

--- src/app/api/suggestionPagePost/[id]/route.ts
+++ src/app/api/suggestionPagePost/[id]/route.ts
@@ -5,21 +5,16 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: NextRequest, { params }: any) {
     const { id } = params;
     const { title, description, category, authorId } = await request.json();
+    const newSuggestionPagePost = new SuggestionPage({
+        title,
+        description,
+        category,
+        authorId: id,
+    });
+    console.log(newSuggestionPagePost);
     await connectToDB();
     try {
-        const suggestionPagePost = await SuggestionPage.findOne({ _id: id });
-        if (!suggestionPagePost) {
-            return NextResponse.json(
-                { message: 'Suggestion Post not found' },
-                { status: 404 },
-            );
-        }
-        await SuggestionPage.findByIdAndUpdate(id, {
-            title,
-            description,
-            category,
-            authorId,
-        });
+        await SuggestionPage.create(newSuggestionPagePost);
         return NextResponse.json(
             { message: 'Suggestion Post updated successfully' },
             { status: 200 },
