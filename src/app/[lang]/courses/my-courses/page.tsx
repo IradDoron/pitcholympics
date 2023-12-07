80fef9cab37fe63f607d6d126fcc34e652ca2e33
Irad Doron <irad.doron@gmail.com>
2023-12-07 13:21:56 +0200

    add routing

--- src/app/[lang]/courses/my-courses/page.tsx
+++ src/app/[lang]/courses/my-courses/page.tsx
@@ -1,19 +1,33 @@
 'use client';
 
 import { Button } from '@/components/core';
+import Text from '@/components/core/Text';
 import CourseCard from '@/components/shared/courseCard';
+import { Locale } from '@/i18n.config';
 import courses from '@/mockData/courses';
+import { useRouter } from 'next/navigation';
 
-const Page = () => {
-    const handleCreateNewCourseClick = () => {};
+type Props = {
+    params: {
+        lang: Locale;
+    };
+};
+
+const Page = ({ params }: Props) => {
+    const router = useRouter();
+    const { lang } = params;
+    const handleCreateNewCourseClick = () => {
+        router.push(`/${lang}/courses/my-courses/create-new-course/info`);
+    };
     return (
-        <div>
-            <h1>My Courses Page</h1>
+        <div className='flex flex-col gap-6'>
+            <Text className='text-3xl text-center'>My Courses Page</Text>
             <Button
                 label='Create new course'
                 onClick={handleCreateNewCourseClick}
+                className='w-fit'
             />
-            <section>
+            <section className='flex gap-4'>
                 {courses.map(course => {
                     const { id } = course;
 
