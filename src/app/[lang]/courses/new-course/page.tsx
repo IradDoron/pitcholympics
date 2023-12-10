d6ddf524d254e2b153ebf9e523f9aaf1c9defe12
pelegsch666 <peleg.mazuz@gmail.com>
2023-12-10 10:36:13 +0200

    finished

--- src/app/[lang]/courses/new-course/page.tsx
+++ src/app/[lang]/courses/new-course/page.tsx
@@ -14,17 +14,17 @@ const Page = ({ params }: Props) => {
         <div className='flex flex-col items-center justify-center gap-6'>
             <Text className='text-xl'>מה תרצו ללמוד?</Text>
 
-            <div className='flex w-[600px] debug  flex-row flex-wrap items-center justify-center h-1/2 gap-20'>
-                <div className='w-[200px] h-[130px] bg-blue-500 debug flex items-center justify-center'>
+            <div className='flex w-[600px]   flex-row flex-wrap items-center justify-center h-1/2 gap-20'>
+                <div className='w-[200px] h-[130px] bg-blue-500  flex items-center justify-center'>
                     <Text className='text-sm'> פסנתר</Text>
                 </div>
-                <div className='w-[200px] h-[130px] bg-blue-500 debug flex items-center justify-center'>
+                <div className='w-[200px] h-[130px] bg-blue-500  flex items-center justify-center'>
                     <Text className='text-sm'> גיטרה</Text>
                 </div>
-                <div className='w-[200px] h-[130px] bg-blue-500 debug flex items-center justify-center'>
+                <div className='w-[200px] h-[130px] bg-blue-500  flex items-center justify-center'>
                     <Text className='text-sm'> תופים</Text>
                 </div>
-                <div className='w-[200px] h-[130px] bg-blue-500 debug flex items-center justify-center'>
+                <div className='w-[200px] h-[130px] bg-blue-500  flex items-center justify-center'>
                     <Text className='text-sm'> קיובייס</Text>
                 </div>
             </div>
