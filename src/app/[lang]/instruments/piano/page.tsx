aad48ffd0affc3b676b88cfa4a3956cc3597e423
Irad Doron <irad.doron@gmail.com>
2023-12-12 23:15:52 +0200

    finish piano navigation

--- src/app/[lang]/instruments/piano/page.tsx
+++ src/app/[lang]/instruments/piano/page.tsx
@@ -11,33 +11,7 @@ const Page = ({ params }: Props) => {
     const { lang } = params;
     return (
         <div>
-            <h1 className='text-2xl font-bold'>Learn to play the piano</h1>
-            <ul className='flex gap-4 m-4'>
-                <li>
-                    <Link
-                        url={`/${lang}/instruments/piano/accompaniment-patterns`}
-                        label='Accompaniment patterns'
-                    />
-                </li>
-                <li>
-                    <Link
-                        url={`/${lang}/instruments/piano/solos`}
-                        label='Solos'
-                    />
-                </li>
-                <li>
-                    <Link
-                        url={`/${lang}/instruments/piano/songs`}
-                        label='Songs'
-                    />
-                </li>
-                <li>
-                    <Link
-                        url={`/${lang}/instruments/piano/technique`}
-                        label='Technique'
-                    />
-                </li>
-            </ul>
+           
         </div>
     );
 };
