aefb41266a3b8ee3c83f7cd1fc166408f3655f28
Amit Basher <amitb2319@gmail.com>
2023-12-12 17:13:15 +0200

    swiching from use-sound to Tone JS

--- src/components/shared/pianoChords/comp.tsx
+++ src/components/shared/pianoChords/comp.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React,{ useRef, useState } from 'react';
-import useSound from 'use-sound';
+import React,{ useState } from 'react';
+// import useSound from 'use-sound';
+import * as Tone from 'tone'
 import ISoundsMap from '@/models/ISoundMap';
 import PianoSoundMap from '@/models/SoundsMap/piano-sound-map';
 interface IKeyboardPianoMap{
@@ -71,20 +72,19 @@ class SoundConfig {
     volume : number = 1;
     sound : ISoundsMap = new PianoSoundMap;
     private keyboard : keyboardMapper = new keyboardMapper;
-    get Keyboard(): IKeyboardPianoMap{
-        return this.keyboard.Piano
+    KeyBoard : IKeyboardPianoMap = this.keyboard.Piano;
+    
+    KeyboardUp(){
+        this.KeyBoard = this.keyboard.MapUp();
     }
-    get KeyboardUp() : IKeyboardPianoMap{
-        return this.keyboard.MapUp();
+    KeyboardDown(){
+        this.KeyBoard = this.keyboard.MapDown();
     }
-    get KeyboardDown() : IKeyboardPianoMap{
-        return this.keyboard.MapDown();
+    KeyboardLeft(){
+        this.KeyBoard = this.keyboard.MapLeft();
     }
-    get KeyboardLeft() : IKeyboardPianoMap{
-        return this.keyboard.MapLeft();
-    }
-    get KeyboardRight() : IKeyboardPianoMap{
-        return this.keyboard.MapRight();
+     KeyboardRight(){
+        this.KeyBoard = this.keyboard.MapRight();
     }
 }
 const Comp = () => {
@@ -95,51 +95,68 @@ const Comp = () => {
         temp[key] = bool;
         setDic(temp);
     }
-    const [playC]  = useSound(config.sound.C,{volume:config.volume});
-    const [playCd] = useSound(config.sound.Cd,{volume:config.volume});
-    const [playD]  = useSound(config.sound.D,{volume:config.volume});
-    const [playDd] = useSound(config.sound.Dd,{volume:config.volume});
-    const [playE]  = useSound(config.sound.E,{volume:config.volume});
-    const [playF]  = useSound(config.sound.F,{volume:config.volume});
-    const [playFd] = useSound(config.sound.Fd,{volume:config.volume});
-    const [playG]  = useSound(config.sound.G,{volume:config.volume});
-    const [playGd] = useSound(config.sound.Gd,{volume:config.volume});
-    const [playA]  = useSound(config.sound.A,{volume:config.volume});
-    const [playAd] = useSound(config.sound.Ad,{volume:config.volume});
-    const [playB]  = useSound(config.sound.B,{volume:config.volume});
-    const [playC2] = useSound(config.sound.C2,{volume:config.volume});
+    // const sampler = new Tone.Sampler({
+    //     urls:{
+    //         C:""
+    //     },
+    //     onload: () => {
+    //         sampler.triggerAttackRelease(["C"], 0.5);
+    //     }
+    // }).toDestination();
+    const sampler = new Tone.Sampler({
+        urls:{
+            C1:"piano_C3.mp3",
+        },
+        baseUrl:"/sounds/piano/"
+    }).toDestination();
+    //const [playC]  = useSound(config.sound.C, {volume:config.volume});
+    // const [playCd] = useSound(config.sound.Cd,{volume:config.volume});
+    // const [playD]  = useSound(config.sound.D, {volume:config.volume});
+    // const [playDd] = useSound(config.sound.Dd,{volume:config.volume});
+    // const [playE]  = useSound(config.sound.E, {volume:config.volume});
+    // const [playF]  = useSound(config.sound.F, {volume:config.volume});
+    // const [playFd] = useSound(config.sound.Fd,{volume:config.volume});
+    // const [playG]  = useSound(config.sound.G, {volume:config.volume});
+    // const [playGd] = useSound(config.sound.Gd,{volume:config.volume});
+    // const [playA]  = useSound(config.sound.A, {volume:config.volume});
+    // const [playAd] = useSound(config.sound.Ad,{volume:config.volume});
+    // const [playB]  = useSound(config.sound.B, {volume:config.volume});
+    // const [playC2] = useSound(config.sound.C2,{volume:config.volume});
 
-    const onClickZ=()=>playC();
-    const onClickS=()=>playCd();
-    const onClickX=()=>playD();
-    const onClickD=()=>playDd();
-    const onClickC=()=>playE();
-    const onClickV=()=>playF();
-    const onClickG=()=>playFd();
-    const onClickB=()=>playG();
-    const onClickH=()=>playGd();
-    const onClickN=()=>playA();
-    const onClickJ=()=>playAd();
-    const onClickM=()=>playB();
-    const onClickComma=()=>playC2();
+
+    const onClickZ=() => sampler.triggerAttackRelease(["C1"], 1);
+
+    // const onClickZ=()=>playC();
+    // const onClickS=()=>playCd();
+    // const onClickX=()=>playD();
+    // const onClickD=()=>playDd();
+    // const onClickC=()=>playE();
+    // const onClickV=()=>playF();
+    // const onClickG=()=>playFd();
+    // const onClickB=()=>playG();
+    // const onClickH=()=>playGd();
+    // const onClickN=()=>playA();
+    // const onClickJ=()=>playAd();
+    // const onClickM=()=>playB();
+    // const onClickComma=()=>playC2();
     
     const playKey = (key:string) =>{
-        console.log(config.Keyboard.C);
+        console.log(config.KeyBoard.C);
         console.log(key);
         switch(key){
-            case config.Keyboard.C: document.getElementById('c')?.click();  break;
-            case config.Keyboard.Cd: document.getElementById('cd')?.click(); break;
-            case config.Keyboard.D: document.getElementById('d')?.click(); break;
-            case config.Keyboard.Dd: document.getElementById('dd')?.click(); break;
-            case config.Keyboard.E: document.getElementById('e')?.click(); break;
-            case config.Keyboard.F: document.getElementById('f')?.click(); break;
-            case config.Keyboard.Fd: document.getElementById('fd')?.click(); break;
-            case config.Keyboard.G: document.getElementById('g')?.click(); break;
-            case config.Keyboard.Gd: document.getElementById('gd')?.click(); break;
-            case config.Keyboard.A: document.getElementById('a')?.click(); break;
-            case config.Keyboard.Ad: document.getElementById('ad')?.click(); break;
-            case config.Keyboard.B: document.getElementById('b')?.click(); break;
-            case config.Keyboard.C2: document.getElementById('c2')?.click(); break;
+            case config.KeyBoard.C: document.getElementById('c')?.click(); break;
+            // case config.KeyBoard.Cd: document.getElementById('cd')?.click(); break;
+            // case config.KeyBoard.D: document.getElementById('d')?.click(); break;
+            // case config.KeyBoard.Dd: document.getElementById('dd')?.click(); break;
+            // case config.KeyBoard.E: document.getElementById('e')?.click(); break;
+            // case config.KeyBoard.F: document.getElementById('f')?.click(); break;
+            // case config.KeyBoard.Fd: document.getElementById('fd')?.click(); break;
+            // case config.KeyBoard.G: document.getElementById('g')?.click(); break;
+            // case config.KeyBoard.Gd: document.getElementById('gd')?.click(); break;
+            // case config.KeyBoard.A: document.getElementById('a')?.click(); break;
+            // case config.KeyBoard.Ad: document.getElementById('ad')?.click(); break;
+            // case config.KeyBoard.B: document.getElementById('b')?.click(); break;
+            // case config.KeyBoard.C2: document.getElementById('c2')?.click(); break;
         }
     }
     const onKeyDown = (event:KeyboardEvent) =>{
@@ -159,8 +176,12 @@ const Comp = () => {
             console.log("Key Up:" + event.key);
         }
     }
-    const changeKeyboardMap = () =>{
-
+    const changeKeyboardMap = (direction:number) =>{
+        switch(direction){
+            case 0: 
+                config.KeyboardUp();
+                break;
+        }
     }
     React.useEffect(() => {
         addEventListener('keydown', onKeyDown,{once:false,passive:false,capture:false,});
@@ -174,11 +195,11 @@ const Comp = () => {
     return(
         <div className='h-full flex justify-center items-center'>   
             <div>
-                <button id='y' className='m-5' onClick={changeKeyboardMap}>Up</button>
+                {/* <button id='y' className='m-5' onClick={()=>{changeKeyboardMap(0);}}>Up</button> */}
             </div>
             <div className='h-full flex justify-center items-center'>
-                <button id='c' className='m-5' onClick={onClickZ}>Play C</button>
-                <button id='cd' className='m-5' onClick={onClickS}>Play C#</button>
+            <button id='c' className='m-5' onClick={onClickZ}>Play C</button>
+                {/* <button id='cd' className='m-5' onClick={onClickS}>Play C#</button>
                 <button id='d' className='m-5' onClick={onClickX}>Play D</button>
                 <button id='dd' className='m-5' onClick={onClickD}>Play D#</button>
                 <button id='e' className='m-5' onClick={onClickC}>Play E</button>
@@ -189,7 +210,7 @@ const Comp = () => {
                 <button id='a' className='m-5' onClick={onClickN}>Play A</button>
                 <button id='ad' className='m-5' onClick={onClickJ}>Play A#</button>
                 <button id='b' className='m-5' onClick={onClickM}>Play B</button>
-                <button id='c2' className='m-5' onClick={onClickComma}>Play High C</button>
+                <button id='c2' className='m-5' onClick={onClickComma}>Play High C</button> */}
             </div>
 
         </div>
