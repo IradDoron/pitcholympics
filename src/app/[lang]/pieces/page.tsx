e0718828acf8e05e23d87f0921e8376af6f21dfc
Irad Doron <irad.doron@gmail.com>
2023-12-13 22:18:58 +0200

    add layout

--- src/app/[lang]/pieces/page.tsx
+++ src/app/[lang]/pieces/page.tsx
@@ -1,31 +1,7 @@
-import { Locale } from '@/i18n.config';
-import { Link } from '@core';
-
-type Props = {
-    params: {
-        lang: Locale;
-    };
-};
-
-const Page = ({ params }: Props) => {
-    const { lang } = params;
+const Page = () => {
     return (
         <>
             <h1>Pieces</h1>
-            <ul className='flex gap-4 m-4'>
-                <li>
-                    <Link
-                        url={`/${lang}/pieces/search-pieces`}
-                        label='Search Pieces'
-                    />
-                </li>
-                <li>
-                    <Link
-                        url={`/${lang}/pieces/create-piece`}
-                        label='Create Piece'
-                    />
-                </li>
-            </ul>
         </>
     );
 };
