84ffc8f3995760a2320b034018126e0a24a72900
pelegsch666 <peleg.mazuz@gmail.com>
2023-12-17 20:17:37 +0200

    done

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,12 @@
+'use client';
+import { createGlobalStyle, css } from 'styled-components';
+
+
+const styles = css`
+  // your global styles
+`;
+const GlobalStyles = createGlobalStyle`
+   ${styles}
+`;
+
+export default GlobalStyles;
