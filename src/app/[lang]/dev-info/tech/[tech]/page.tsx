c2a83632536a4034ad1725d01c0ac8cfe654f9db
Irad Doron <irad.doron@gmail.com>
2023-12-19 16:58:49 +0200

    add drive links

--- src/app/[lang]/dev-info/tech/[tech]/page.tsx
+++ src/app/[lang]/dev-info/tech/[tech]/page.tsx
@@ -1,3 +1,6 @@
+import { projectTechInfo } from '@/data/projectTechInfo';
+import { Link } from '@core';
+
 type Props = {
     params: {
         tech: string;
@@ -6,9 +9,27 @@ type Props = {
 
 const Page = ({ params }: Props) => {
     const { tech } = params;
+
+    const hasTech = Object.prototype.hasOwnProperty.call(projectTechInfo, tech);
+
+    if (!hasTech) {
+        return (
+            <div>
+                <h1>There is no data for {tech} yet</h1>
+            </div>
+        );
+    }
+
+    const techData = projectTechInfo[tech];
+
     return (
         <div>
             <h1>{tech}</h1>
+            <Link
+                newTab={true}
+                url={techData.driveFolderLink}
+                label={`${tech} Drive Folder`}
+            />
         </div>
     );
 };
---
+++ src/data/projectTechInfo.ts
@@ -0,0 +1,12 @@
+type ProjectTechInfo = {
+    [key: string]: {
+        driveFolderLink: string;
+    };
+};
+
+export const projectTechInfo = {
+    nextjs: {
+        driveFolderLink:
+            'https://drive.google.com/drive/folders/1S6pUCSa7V6SuutCUrezA2LIBHMND9jYy?usp=drive_link',
+    },
+} as ProjectTechInfo;
