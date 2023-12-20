53b4c9ec90f5d4caeb1aa0451f2a78854ff2f1d1
Irad Doron <irad.doron@gmail.com>
2023-12-20 15:21:37 +0200

    add basic page

--- src/app/[lang]/q&a/page.tsx
+++ src/app/[lang]/q&a/page.tsx
@@ -1,7 +1,30 @@
+'use client';
+
+import { Button } from '@core';
+import { useState } from 'react';
+
 const Page = () => {
+    const [currentQuestion, setCurrentQuestion] = useState('');
+
+    const handleQuestionChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement>,
+    ) => {
+        setCurrentQuestion(event.target.value);
+    };
+
+    const handleSubmitClick = () => {
+        console.log(currentQuestion);
+    };
+
     return (
         <div>
             <h1>Q&A</h1>
+            <textarea
+                placeholder='Ask a question'
+                onChange={handleQuestionChange}
+                value={currentQuestion}
+            />
+            <Button label='Submit' onClick={handleSubmitClick} />
         </div>
     );
 };
