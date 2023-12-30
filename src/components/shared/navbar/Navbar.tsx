63d6d7c5799ab5c4c0024fb4a3ef76c05bc8bdad
Irad Doron <irad.doron@gmail.com>
2023-12-30 14:55:01 +0200

    add mvp toggle

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Accessibility, SettingsMenu } from '@shared';
+import { Button } from '@core';
+import { SettingsMenu } from '@shared';
 import { LangParam } from '@types';
 import { getDictionaryClient } from '@utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthButton } from './AuthButton';
 import { LocaleSwitcher } from './LocaleSwitcher';
 import { NavbarLink } from './NavbarLink';
@@ -12,16 +13,28 @@ import { MobileNav } from './mobileNav';
 
 export function Navbar({ params: { lang } }: LangParam) {
     const [isSettingsMenuOpen, setIsSettingsMenuOpen] = useState(false);
+    const [isMvp, setIsMvp] = useState(true);
+    const [navPages, setNavPages] = useState<NavPage[]>([]);
     const dict = getDictionaryClient(lang);
     const { pages } = dict.shared.navbar;
 
-    const navPages = [
+    type NavPage = {
+        url: string;
+        label: string;
+    };
+
+    const mvpNavPages = [
         { url: `/${lang}`, label: pages.home },
+        { url: `/${lang}/games`, label: pages.games },
+        { url: `/${lang}/profile`, label: pages.profile },
+        { url: `/${lang}/stats`, label: pages.stats },
+    ] as NavPage[];
+
+    const futureNavPages = [
         { url: `/${lang}/about`, label: pages.about },
         { url: `/${lang}/courses`, label: pages.courses },
         { url: `/${lang}/dev-info/tech`, label: pages.devInfo },
         { url: `/${lang}/faq`, label: pages.faq },
-        { url: `/${lang}/games`, label: pages.games },
         { url: `/${lang}/how-to-contribute`, label: pages.howToContribute },
         { url: `/${lang}/instruments`, label: pages.instruments },
         { url: `/${lang}/items`, label: pages.items },
@@ -33,44 +46,62 @@ export function Navbar({ params: { lang } }: LangParam) {
         { url: `/${lang}/pieces`, label: pages.pieces },
         { url: `/${lang}/polls`, label: pages.polls },
         { url: `/${lang}/practice`, label: pages.practice },
-        { url: `/${lang}/profile`, label: pages.profile },
         { url: `/${lang}/q&a`, label: pages['q&a'] },
         { url: `/${lang}/roadmap`, label: pages.roadmap },
         { url: `/${lang}/shop`, label: pages.shop },
-        { url: `/${lang}/stats`, label: pages.stats },
         { url: `/${lang}/suggestions`, label: pages.suggestions },
         { url: `/${lang}/website-analytics`, label: pages.websiteAnalytics },
         { url: `/${lang}/teaching`, label: pages.teaching },
-    ];
+    ] as NavPage[];
+
+    const handleNavPagesToggleClick = () => {
+        setIsMvp(!isMvp);
+    };
+
+    useEffect(() => {
+        if (isMvp) {
+            setNavPages(mvpNavPages);
+        } else {
+            const allNavPages = [...mvpNavPages, ...futureNavPages];
+            setNavPages(allNavPages);
+        }
+    }, [isMvp]);
 
     return (
-        <div className=' flex flex-row justify-center'>
-            <Accessibility />
-            <nav className='flex flex-row p-2 sticky w-full bg-light-background-defaultBlur dark:bg-dark-background-defaultBlur '>
-                <div className='flex justify-self-start px-10 w-fit'>
-                    <LocaleSwitcher params={{ lang }} />
-                    <div className='flex-between gap-2'>
-                        <Theme />
+        <div className='flex flex-col'>
+            <Button
+                label={isMvp ? 'MVP' : 'All'}
+                onClick={handleNavPagesToggleClick}
+                className='w-20 m-auto'
+            />
+
+            <div className=' flex flex-row justify-center'>
+                <nav className='flex flex-row p-2 sticky w-full bg-light-background-defaultBlur dark:bg-dark-background-defaultBlur '>
+                    <div className='flex justify-self-start px-10 w-fit'>
+                        <LocaleSwitcher params={{ lang }} />
+                        <div className='flex-between gap-2'>
+                            <Theme />
+                        </div>
                     </div>
-                </div>
-                <ul className='gap-5 hidden sm:flex sm:flex-wrap sm:w-full justify-self-center'>
-                    {navPages.map(({ url, label }) => (
-                        <NavbarLink key={url} url={url} label={label} />
-                    ))}
-                </ul>
-                <div className='flex justify-self-end pe-10 relative w-fit'>
-                    <AuthButton
-                        isSettingsMenuOpen={isSettingsMenuOpen}
-                        setIsSettingsMenuOpen={setIsSettingsMenuOpen}
-                    />
-                    <SettingsMenu
-                        isOpen={isSettingsMenuOpen}
-                        setIsOpen={setIsSettingsMenuOpen}
-                        lang={lang}
-                    />
-                </div>
-                <MobileNav lang={lang} />
-            </nav>
+                    <ul className='gap-5 hidden sm:flex sm:flex-wrap sm:w-full justify-self-center'>
+                        {navPages.map(({ url, label }) => (
+                            <NavbarLink key={url} url={url} label={label} />
+                        ))}
+                    </ul>
+                    <div className='flex justify-self-end pe-10 relative w-fit'>
+                        <AuthButton
+                            isSettingsMenuOpen={isSettingsMenuOpen}
+                            setIsSettingsMenuOpen={setIsSettingsMenuOpen}
+                        />
+                        <SettingsMenu
+                            isOpen={isSettingsMenuOpen}
+                            setIsOpen={setIsSettingsMenuOpen}
+                            lang={lang}
+                        />
+                    </div>
+                    <MobileNav lang={lang} />
+                </nav>
+            </div>
         </div>
     );
 }
