323907aca4bb2a1f63a94f777b988b54b279575a
Irad Doron <irad.doron@gmail.com>
2024-01-01 10:41:57 +0200

    fix eslint files

--- src/components/core/LevelLink.tsx
+++ src/components/core/LevelLink.tsx
@@ -1,3 +1,10 @@
+import {
+    Button as BaseButton,
+    ButtonClasses,
+    buttonClasses,
+} from '@mui/base/Button';
+import { Button as MaterialButton } from '@mui/material';
+import { styled } from '@mui/system';
 import Link from 'next/link';
 
 type Props = {
@@ -5,6 +12,33 @@ type Props = {
     state: 'passed' | 'current' | 'failed' | 'locked';
     level: number;
 };
+
+type ButtonProps = {
+    myClasses?: string;
+} & ButtonClasses;
+
+const Button = styled(BaseButton)(
+    ({ theme }) => `
+    ${buttonClasses.root} {
+        min-width: 0;
+        padding: 0;
+        width: 2rem;
+        height: 2rem;
+        border-radius: 50%;
+    }
+    &.locked {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 9999px;
+        width: 1.25rem;
+        height: 1.25rem;
+        text-align: center;
+        color: #ffffff;
+        background-color: #6b7280;
+    }
+`,
+);
 
 function LevelLink({ url, state, level }: Props) {
     return (
--- src/components/core/NotesCircle/NotesCircle.tsx
+++ src/components/core/NotesCircle/NotesCircle.tsx
@@ -3,15 +3,24 @@ type Props = {
 };
 
 export const NotesCircle = ({ state }: Props) => {
+    let color = '';
+
+    switch (state) {
+        case 'played':
+            color = 'bg-green-500';
+            break;
+        case 'notPlayed':
+            color = 'bg-gray-500';
+            break;
+        case 'mainNote':
+            color = 'bg-blue-500';
+            break;
+        default:
+            color = 'bg-gray-500';
+    }
+
     return (
-        (state === 'played' && (
-            <div className='w-32 h-32 rounded-full bg-green-500'></div>
-        )) ||
-        (state === 'notPlayed' && (
-            <div className='w-32 h-32 rounded-full bg-gray-500 '></div>
-        )) ||
-        (state === 'mainNote' && (
-            <div className='w-32 h-32 rounded-full bg-blue-500 '></div>
-        ))
+        <div
+            className={`w-[30px] h-[30px] rounded-full ${color} border-2 border-black`}></div>
     );
 };
